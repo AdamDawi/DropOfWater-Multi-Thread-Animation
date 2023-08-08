e65160f84547563edd45717fb62a816193a7bdd5
Adam Dawidziuk <adamdawidziuk@onet.pl>
2023-08-08 22:03:03 +0200

    Final commit Animation

--- src/animation/AnimationFrame.java
+++ src/animation/AnimationFrame.java
@@ -1,16 +1,19 @@
+package animation;
 import java.awt.*;
 import javax.swing.*;
 
 public class AnimationFrame extends JFrame
 {
+    public AnimationPanel animationPanel = new AnimationPanel();
+
     AnimationFrame()
     {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setIconImage(new ImageIcon("water.jpg").getImage());
         this.setLocationRelativeTo(null);
         this.setTitle("Animation");
-        this.getContentPane().add(new AnimationPanel());
-        this.getContentPane().add(new ButtonPanel(), BorderLayout.SOUTH);
+        this.getContentPane().add(animationPanel);
+        this.getContentPane().add(new ButtonPanel(animationPanel), BorderLayout.SOUTH);
         this.pack();
         this.setVisible(true);
     }
--- src/animation/ButtonPanel.java
+++ src/animation/ButtonPanel.java
@@ -1,26 +1,32 @@
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
+package animation;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class ButtonPanel extends JPanel
 {
     private JButton startButton = new JButton("Start");
     private JButton stopButton = new JButton("Stop");
-    ButtonPanel()
+
+    ButtonPanel(AnimationPanel animationPanel)
     {
         startButton.setPreferredSize(new Dimension(100, 30));
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) 
             {
-                
+                animationPanel.addDropOfWater();
             }
         });
 
         stopButton.setPreferredSize(new Dimension(100, 30));
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                animationPanel.stop();
+            }
+        });
 
         this.add(startButton);
         this.add(stopButton);
